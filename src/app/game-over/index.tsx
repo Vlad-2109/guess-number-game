64768317660a5186a7ca6d26b9fe7091fcebe02d
clouden <clouden@syneforge.com>
2026-07-22 11:34:55 +0300

    game over screen styles improvements

--- src/app/game-over/index.tsx
+++ src/app/game-over/index.tsx
@@ -1,6 +1,14 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { router, useLocalSearchParams } from 'expo-router';
-import { Image, ImageBackground, StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+	Image,
+	ImageBackground,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+	useWindowDimensions,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { COLORS } from '@/constants/colors';
@@ -10,6 +18,25 @@ import Title from '@/components/ui/Title';
 
 export default function GameOverScreen() {
 	const { roundsNumber, userNumber } = useLocalSearchParams();
+	const { width, height } = useWindowDimensions();
+
+	let imageSize = 300;
+
+	if (width < 380) {
+		imageSize = 150;
+	}
+
+	if (height < 410) {
+		imageSize = 150;
+	}
+
+	const imageStyle = {
+		width: imageSize,
+		height: imageSize,
+		borderRadius: imageSize / 2,
+	};
+
+	const paddingTopDistance = height < 410 ? 10 : 0;
 
 	const startNewGame = () => {
 		router.push('/');
@@ -26,27 +53,28 @@ export default function GameOverScreen() {
 				imageStyle={styles.image}
 				style={styles.imageBackground}
 			>
-				<SafeAreaView style={styles.container}>
-					<Title>Game is over!</Title>
-					<View style={styles.imageContainer}>
-						<Image
-							source={require('@/assets/images/success.png')}
-							style={styles.imageSuccess}
-						/>
-					</View>
-					<Text style={styles.summaryText}>
-						Your phone needed{' '}
-						<Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess
-						the number <Text style={styles.highlight}>{userNumber}</Text>.
-					</Text>
-					<PrimaryButton onPress={startNewGame}>Start New Game</PrimaryButton>
-				</SafeAreaView>
+				<ScrollView style={styles.screen}>
+					<SafeAreaView style={[styles.container, {paddingTop: paddingTopDistance}]}>
+						<Title>Game is over!</Title>
+						<View style={[styles.imageContainer, imageStyle]}>
+							<Image
+								source={require('@/assets/images/success.png')}
+								style={styles.imageSuccess}
+							/>
+						</View>
+						<Text style={styles.summaryText}>
+							Your phone needed{' '}
+							<Text style={styles.highlight}>{roundsNumber}</Text> rounds to
+							guess the number{' '}
+							<Text style={styles.highlight}>{userNumber}</Text>.
+						</Text>
+						<PrimaryButton onPress={startNewGame}>Start New Game</PrimaryButton>
+					</SafeAreaView>
+				</ScrollView>
 			</ImageBackground>
 		</LinearGradient>
 	);
 }
-
-const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
 	gradientContainer: {
@@ -55,19 +83,20 @@ const styles = StyleSheet.create({
 	imageBackground: {
 		flex: 1,
 	},
+	screen: {
+		flex: 1,
+	},
 	image: {
 		opacity: 0.15,
 	},
 	container: {
 		flex: 1,
+		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
 	imageContainer: {
-		width: deviceWidth < 380 ? 150 : 300,
-		height: deviceWidth < 380 ? 150 : 300,
 		marginVertical: 36,
-		borderRadius: deviceWidth < 380 ? 75 : 150,
 		borderWidth: 3,
 		borderColor: COLORS.primary800,
 		overflow: 'hidden',
