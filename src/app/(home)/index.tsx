229ac097937f3707e216205b6ac7912beb62fd24
clouden <clouden@syneforge.com>
2026-07-22 11:57:36 +0300

    style status bar

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,5 +1,6 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import {
 	Alert,
@@ -58,49 +59,52 @@ const StartGame = () => {
 	};
 
 	return (
-		<LinearGradient
-			colors={[COLORS.primary700, COLORS.accent500]}
-			style={styles.gradientContainer}
-		>
-			<ImageBackground
-				source={require('@/assets/images/background.png')}
-				resizeMode="cover"
-				imageStyle={styles.image}
-				style={styles.imageBackground}
+		<>
+			<StatusBar style="light" />
+			<LinearGradient
+				colors={[COLORS.primary700, COLORS.accent500]}
+				style={styles.gradientContainer}
 			>
-				<ScrollView style={styles.screen}>
-					<KeyboardAvoidingView style={styles.screen} behavior="position">
-						<SafeAreaView
-							style={[styles.container, { paddingTop: paddingTopDistance }]}
-						>
-							<Title>Guess My Number</Title>
-							<Card>
-								<InstructionText>Enter a number</InstructionText>
-								<TextInput
-									value={enteredNumber}
-									onChangeText={handleNumberInput}
-									style={styles.numberInput}
-									maxLength={2}
-									keyboardType="number-pad"
-									autoCapitalize="none"
-									autoCorrect={false}
-								/>
-								<View style={styles.buttonsContainer}>
-									<View style={styles.buttonContainer}>
-										<PrimaryButton onPress={resetInput}>Reset</PrimaryButton>
+				<ImageBackground
+					source={require('@/assets/images/background.png')}
+					resizeMode="cover"
+					imageStyle={styles.image}
+					style={styles.imageBackground}
+				>
+					<ScrollView style={styles.screen}>
+						<KeyboardAvoidingView style={styles.screen} behavior="position">
+							<SafeAreaView
+								style={[styles.container, { paddingTop: paddingTopDistance }]}
+							>
+								<Title>Guess My Number</Title>
+								<Card>
+									<InstructionText>Enter a number</InstructionText>
+									<TextInput
+										value={enteredNumber}
+										onChangeText={handleNumberInput}
+										style={styles.numberInput}
+										maxLength={2}
+										keyboardType="number-pad"
+										autoCapitalize="none"
+										autoCorrect={false}
+									/>
+									<View style={styles.buttonsContainer}>
+										<View style={styles.buttonContainer}>
+											<PrimaryButton onPress={resetInput}>Reset</PrimaryButton>
+										</View>
+										<View style={styles.buttonContainer}>
+											<PrimaryButton onPress={confirmInput}>
+												Confirm
+											</PrimaryButton>
+										</View>
 									</View>
-									<View style={styles.buttonContainer}>
-										<PrimaryButton onPress={confirmInput}>
-											Confirm
-										</PrimaryButton>
-									</View>
-								</View>
-							</Card>
-						</SafeAreaView>
-					</KeyboardAvoidingView>
-				</ScrollView>
-			</ImageBackground>
-		</LinearGradient>
+								</Card>
+							</SafeAreaView>
+						</KeyboardAvoidingView>
+					</ScrollView>
+				</ImageBackground>
+			</LinearGradient>
+		</>
 	);
 };
 
